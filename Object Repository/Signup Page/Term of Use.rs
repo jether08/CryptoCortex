45970cb9b70e3b16b8714efc6b87f04238cb7456
arcillas <arcillas@arcillas-PC>
2018-10-27 11:18:32 +0800

    Update Dev profile, dqrm-4 and signup page object repo

---
+++ Object Repository/Signup Page/Term of Use.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Term of Use</name>
+   <tag></tag>
+   <elementGuidId>9b42151c-f3e2-438a-a802-6b2866bb10d3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id='I agree to the Terms of Use']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id='I agree to the Terms of Use']</value>
+   </webElementProperties>
+</WebElementEntity>
--- Scripts/Sign Up/DQRM-4/Script1537876508217.groovy
+++ Scripts/Sign Up/DQRM-4/Script1537876508217.groovy
@@ -20,8 +20,9 @@ import org.openqa.selenium.By
 
 WebUI.openBrowser('')
 
+
 //Navigate to Sign Up page
-WebUI.navigateToUrl(GlobalVariable.SignupUrl)
+WebUI.navigateToUrl(GlobalVariable.newSingupUrl)
 
 WebUI.waitForPageLoad(10)
 
@@ -54,42 +55,53 @@ WebUI.delay(3)
 
 
 //Enter given name
-WebUI.setText(findTestObject('Signup Page/Given Name'), 'Dummy1')
+//WebUI.setText(findTestObject('Signup Page/Given Name'), 'Dummy1')
 
 //Enter middle name
-WebUI.setText(findTestObject('Signup Page/Middle Name'), 'Middle')
+//WebUI.setText(findTestObject('Signup Page/Middle Name'), 'Middle')
 
 //Enter Surname
-WebUI.setText(findTestObject('Signup Page/Surname'), 'Test')
+//WebUI.setText(findTestObject('Signup Page/Surname'), 'Test')
 
 
 //Enter email address
 WebUI.setText(findTestObject('Signup Page/Email'), IDmail)
 
 //Enter Phone number
-WebUI.setText(findTestObject('Signup Page/Phone'), '0109001')
+//WebUI.setText(findTestObject('Signup Page/Phone'), '0109001')
 
 //Enter Country: Open the country dropdown, select United States
 WebUI.click(findTestObject('Signup Page/Country button'))
 driver.findElement(By.xpath('//div[contains(text(),"United States")]')).click()
 
 //Enter State
-WebUI.setText(findTestObject('Signup Page/State'), "NY")
+//WebUI.setText(findTestObject('Signup Page/State'), "NY")
 
 //Enter City
-WebUI.setText(findTestObject('Signup Page/City'), "New York")
+//WebUI.setText(findTestObject('Signup Page/City'), "New York")
 
 //enter Street address
-WebUI.setText(findTestObject('Signup Page/Street'), "123 St.")
+//WebUI.setText(findTestObject('Signup Page/Street'), "123 St.")
 
 //Enter Postal code
-WebUI.setText(findTestObject('Signup Page/PostCode'), "8888")
+//WebUI.setText(findTestObject('Signup Page/PostCode'), "8888")
 
 //Enter Password
-WebUI.setMaskedText(findTestObject('Signup Page/Password'), "dqr123456!")
+WebUI.setMaskedText(findTestObject('Signup Page/Password'), "dqr-private.com")
 
 //Enter Confirm Password
-WebUI.setMaskedText(findTestObject('Signup Page/Confirm Password'), "dqr123456!")
+WebUI.setMaskedText(findTestObject('Signup Page/Confirm Password'), "dqr-private.com")
+
+WebUI.delay(3)
+
+//Check Terms of Use checkbox
+driver.findElement(By.xpath("//form/div[9]/div[1]/div/div[1]/div/div/label/div")).click();
+
+//Check Private Policy checkbox
+driver.findElement(By.xpath('//form/div[9]/div[1]/div/div[2]/div/div/label/div')).click()
+
+//Check Years older checkbox
+driver.findElement(By.xpath('//form/div[9]/div[1]/div/div[3]/div/div/label/div')).click()
 
 //Click Signup
 WebUI.click(findTestObject('Signup Page/Signup button'))
